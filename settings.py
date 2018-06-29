6ff3a9c87e5634027f578a49451bc240872e0bd3
shawn <happyshawn88@gmail.com>
2018-06-29 15:08:51 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    """存储设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        
+        #飞船速度
+        self.ship_speed_factor = 5.5
